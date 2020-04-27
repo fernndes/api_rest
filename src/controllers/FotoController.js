90220cba475ac8ab8a6b885269848d0c64ad504e
Trabalho <you@example.com>
2020-04-27 18:13:09 -0300

    Listando alunos com fotos

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -10,11 +10,17 @@ class FotoController {
     return upload(req, res, async (error) => {
       if (error) { return res.status(400).json({ errors: [error.code] }); }
 
-      const { originalname, filename } = req.file;
-      const { aluno_id } = req.body;
-      const foto = await Foto.create({ originalname, filename, aluno_id });
+      try {
+        const { originalname, filename } = req.file;
+        const { aluno_id } = req.body;
+        const foto = await Foto.create({ originalname, filename, aluno_id });
 
-      return res.json(foto);
+        return res.json(foto);
+      } catch (e) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
     });
   }
 }
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -9,7 +9,7 @@ const router = new Router();
 // router.get('/', loginRequired, UserController.index); // Lista usuários
 // router.get('/:id', UserController.show); // Lista usuário
 
-router.post('/', UserController.store);
+// router.post('/', UserController.store); Fechando temporariamente para que ninguem possa se cadastrar sem ter acesso
 router.put('/', loginRequired, UserController.update); // '/:id' não é correto pois permite que um usuário edite dados de outro, portanto o correto é somente '/'
 router.delete('/', loginRequired, UserController.delete); // '/:id' não é correto pois permite que um usuário delete outros, portanto o correto é somente '/'
 
